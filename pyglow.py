93c43fa7a042ec8826f92a8cecaeadd4bea7b2b4
benleb <git@benleb.de>
2013-08-20 15:57:23 +0200

    add gamma correction

--- pyglow.py
+++ pyglow.py
@@ -105,6 +105,16 @@ class PyGlow:
 
     def set_leds(self, leds, value):
 
+        ## gamma correction - thanks to Jon@Pimoroni for the mapping
+        gamma_table = [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,
+        3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7,7,7,7,8,8,8,8,8,8,9,9,9,9,10,10,10,10,
+        10,11,11,11,11,12,12,12,13,13,13,13,14,14,14,15,15,15,16,16,16,17,17,18,18,18,19,19,20,20,20,21,21,22,22,23,23,24,24,
+        25,26,26,27,27,28,29,29,30,31,31,32,33,33,34,35,36,36,37,38,39,40,41,42,42,43,44,45,46,47,48,50,51,52,53,54,55,57,58,
+        59,60,62,63,64,66,67,69,70,72,74,75,77,79,80,82,84,86,88,90,91,94,96,98,100,102,104,107,109,111,114,116,119,122,124,127,
+        130,133,136,139,142,145,148,151,155,158,161,165,169,172,176,180,184,188,192,196,201,205,210,214,219,224,229,234,239,244,250,255]
+        ## pick the gamma-corrected value
+        gc_value = gamma_table[value]
+
         for led in leds:
             if isinstance(led, int) and 1 <= led <= 18 and 0 <= value <= 255:
                 leds = [
@@ -118,7 +128,7 @@ class PyGlow:
                 self.lights_off("usage: set_leds(leds, value) | leds has to be a list of [1-18] or <color>[1-18]")
 
             ## write update value to the ic
-            self.bus.write_byte_data(I2C_ADDR, int(leds[led], 16), value)
+            self.bus.write_byte_data(I2C_ADDR, int(leds[led], 16), gc_value)
 
 
     def update_leds(self):
