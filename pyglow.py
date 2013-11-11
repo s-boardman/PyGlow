3b41f030124257e975609b72ebab7ffa7093b608
serewit <root@serewit.com>
2013-11-11 10:03:47 -0500

    trying pulse_all and pulse_arm

--- pyglow.py
+++ pyglow.py
@@ -62,16 +62,35 @@ class PyGlow:
     def pulse(self, led, value):
         led = [led];
         step = 0
-        while (step <= value):
+        while (step < value):
             self.set_leds(led, step)
             self.update_leds()
             sleep(0.001)
             step += 1
-        while (step >= 0):
+        while (step > 0):
             self.set_leds(led, step)
             self.update_leds()
             sleep(0.001)
-	    step -= 1
+	        step -= 1
+
+    def pulse_all(self, value):
+        if 0 <= value <= 255:
+            leds = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+            self.pulse(leds, value)
+        else:
+            lights_off("usage: pulse_all([0-255])")
+
+    def pulse_arm(self, arm, value):
+        if arm == 1 and 0 <= value <= 255:
+            leds = [1,2,3,4,5,6]
+        elif arm == 2 and 0 <= value <= 255:
+            leds = [7,8,9,10,11,12]
+        elif arm == 3 and 0 <= value <= 255:
+            leds = [13,14,15,16,17,18]
+        else:
+            self.lights_off("usage: pulse_arm([1-3],[0-255])")
+
+        self.pulse(leds, value)
 
 
     def led(self, led, value):
